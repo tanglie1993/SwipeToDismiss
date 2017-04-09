9e999f72322d408b0486b73520d59713190f2d51
tanglie1993 <tanglie23@163.com>
2017-04-09 21:08:51 +0800

    新建activity

---
+++ app/src/main/java/com/example/administrator/myapplication/SwipeDismissViewActivity.java
@@ -0,0 +1,55 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SwipeDismissViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_dismiss_view);
+
+        String[] items = new String[20];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = "Item " + (i + 1);
+        }
+
+        // Set up normal ViewGroup example
+        final ViewGroup dismissableContainer = (ViewGroup) findViewById(R.id.dismissable_container);
+        for (int i = 0; i < items.length; i++) {
+            final Button dismissableButton = new Button(this);
+            dismissableButton.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            dismissableButton.setText("Button " + (i + 1));
+            dismissableButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(SwipeDismissViewActivity.this,
+                            "Clicked " + ((Button) view).getText(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            // Create a generic swipe-to-dismiss touch listener.
+            dismissableButton.setOnTouchListener(new SwipeDismissTouchListener(
+                    dismissableButton,
+                    null,
+                    new SwipeDismissTouchListener.DismissCallbacks() {
+                        @Override
+                        public boolean canDismiss(Object token) {
+                            return true;
+                        }
+
+                        @Override
+                        public void onDismiss(View view, Object token) {
+                            dismissableContainer.removeView(dismissableButton);
+                        }
+                    }));
+            dismissableContainer.addView(dismissableButton);
+        }
+    }
+}
