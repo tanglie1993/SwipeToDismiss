56fedd88189cc6ae70fdce94e13ef38fe2326eb0
tanglie1993 <tanglie23@163.com>
2017-04-11 23:49:16 +0800

    实现view从左边消失

--- app/src/main/java/com/example/administrator/myapplication/MySwipeDismissTouchListener.java
+++ app/src/main/java/com/example/administrator/myapplication/MySwipeDismissTouchListener.java
@@ -1,7 +1,11 @@
 package com.example.administrator.myapplication;
 
+import android.animation.IntEvaluator;
+import android.animation.ValueAnimator;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 
 /**
@@ -26,12 +30,33 @@ public class MySwipeDismissTouchListener implements View.OnTouchListener {
         if(event.getAction() == MotionEvent.ACTION_DOWN){
             initialX = event.getRawX();
         }else if(event.getAction() == MotionEvent.ACTION_MOVE){
-            System.out.println("setTranslationX: " + (event.getRawX() - initialX
-            ));
+            System.out.println("setTranslationX: " + (event.getRawX() - initialX));
             view.setTranslationX(event.getRawX() - initialX);
+        }else if(event.getAction() == MotionEvent.ACTION_UP){
+            exitFromLeft();
         }
 //        view.dispatchGenericMotionEvent(event);
 
         return false;
+    }
+
+    private void exitFromLeft() {
+        ValueAnimator mAnimator = ValueAnimator.ofFloat(0,1);
+        final float startTranslationX = view.getTranslationX();
+        final float targetTranslationX = - view.getMeasuredWidth() - view.getLeft();
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animatorValue = (float) animation.getAnimatedValue();
+                view.setTranslationX(animatorValue * (targetTranslationX - startTranslationX) + startTranslationX);
+            }
+        });
+        //4.为ValueAnimator设置LinearInterpolator
+        mAnimator.setInterpolator(new LinearInterpolator());
+        //5.设置动画的持续时间
+        mAnimator.setDuration(1000);
+        //6.为ValueAnimator设置目标对象并开始执行动画
+        mAnimator.setTarget(view);
+        mAnimator.start();
     }
 }
